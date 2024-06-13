25b15edcee066e7b2a0d3cd3b53cd72ac9c8499a
Rob Jones <delphi.actual@gmail.com>
2024-06-12 21:41:44 -0600

    init

---
+++ src/helpers.ts
@@ -0,0 +1,100 @@
+/*================================================================================================================================*\
+||
+|| Helper Functions
+||
+||
+\*================================================================================================================================*/
+import { execSync } from 'child_process';
+import { writeFile as writeFileFS } from 'fs';
+import fse from 'fs-extra';
+import { promisify } from 'util';
+import { infoLog } from './log.js';
+
+const TAG = 'UTILS';
+const { writeFileSync /*,copyFileSync */ } = fse;
+
+type WriteHook = (fileName: string) => void;
+const writeHooks: WriteHook[] = [];
+
+export function registerWriteHook(hook: WriteHook) {
+  writeHooks.push(hook);
+}
+
+export function writeFile(filename: string, data: any, pretty = false) {
+  if (typeof data === 'object') {
+    data = JSON.stringify(data, null, 2);
+  }
+
+  writeFileSync(filename, data + '\n', 'utf8');
+
+  if (pretty || filename.endsWith('.ts')) {
+    execSync(`pnpm prettier "${filename}" --write`);
+  }
+
+  infoLog(TAG, `${filename} saved.`);
+
+  for (const hook of writeHooks) {
+    hook(filename);
+  }
+}
+
+export function uniqAndSortArray<T>(array: T[]): T[] {
+  const uniq = [...new Set(array)];
+  return uniq.every(isNumberlike) ? uniq.sort((a, b) => Number(a) - Number(b)) : uniq.sort();
+}
+
+function isNumberlike(x: any): x is number | string {
+  return typeof x === 'number' || (typeof x === 'string' && /^\d+$/.test(x));
+}
+
+export function diffArrays(all: any[], exclude: any[]) {
+  return [
+    ...new Set(
+      all.filter(function (x) {
+        if (!exclude.includes(x)) {
+          return true;
+        } else {
+          return false;
+        }
+      }),
+    ),
+  ];
+}
+
+export function sortObject<T extends Record<string, any>>(o: T): T {
+  const sorted: Record<string, any> = {};
+  const a = Object.keys(o).sort();
+
+  for (const key of a) {
+    sorted[key] = o[key];
+  }
+
+  return sorted as T;
+}
+
+export const writeFilePromise = promisify(writeFileFS);
+
+export function downloadFile(url: string, outputPath: string) {
+  return fetch(url)
+    .then((x) => x.arrayBuffer())
+    .then((x) => writeFilePromise(outputPath, Buffer.from(x)));
+}
+
+export function uriToFileName(uri: string) {
+  return uri.substring(uri.lastIndexOf('/') + 1);
+}
+
+export function makeDirIfMissing(dir: string) {
+  if (!fse.existsSync(dir)) {
+    fse.mkdirSync(dir);
+  }
+}
+
+export function toArrayBuffer(buffer: Buffer) {
+  const arrayBuffer = new ArrayBuffer(buffer.length);
+  const view = new Uint8Array(arrayBuffer);
+  for (let i = 0; i < buffer.length; ++i) {
+    view[i] = buffer[i];
+  }
+  return arrayBuffer;
+}
---
+++ src/main.ts
@@ -0,0 +1,111 @@
+import { spawnSync } from 'child_process';
+import { readdirSync } from 'fs';
+// import fse from 'fs-extra';
+import path, { basename, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { registerWriteHook } from './helpers.js';
+import { infoLog, infoTable } from './log.js';
+
+const TAG = 'MAIN';
+// const { copyFileSync } = fse;
+
+const scriptRegex = /generate-([a-zA-Z\\-]+)\.ts/;
+
+const defaultExcludedScripts = ['pretty-manifest', 'font-glyph-enums'];
+
+// These scripts generate data needed by other scripts,
+// so they need to run first in this order
+const prioritizedScripts = [''];
+// If a script outputs one of these files, compile it
+const toCompileOutputs = ['d2-font-glyphs.ts'];
+const outputDirectories = ['data', 'output'];
+// These files should be copied verbatim from data/ to output/
+//const copyDataToOutput = [''];
+
+// Read all `generate-` files
+const scriptsDir = dirname(fileURLToPath(import.meta.url));
+const projectRootDir = path.join(scriptsDir, '..', '..');
+const sourceDir = path.join(projectRootDir, 'src');
+
+const argvScripts = process.argv.length > 2 ? process.argv.slice(2) : [];
+
+let tsFiles = readdirSync(sourceDir).filter((file) => {
+  const match = basename(file).match(scriptRegex);
+  // let user run any script they desire
+  return match && (argvScripts.length || !defaultExcludedScripts.includes(match[1]));
+});
+// The user can restrict the scripts that should run via the command line
+if (argvScripts.length) {
+  tsFiles = tsFiles.filter((script) => argvScripts.some((pattern) => script.includes(pattern)));
+}
+
+// Sort files so that prioritized files are in the order they appear in `prioritizedScripts`,
+// and everything else comes alphabetically after
+tsFiles.sort((fileA, fileB) => {
+  const matchA = fileA.match(scriptRegex);
+  const matchB = fileB.match(scriptRegex);
+  if (matchA && !matchB) {
+    return -1;
+  } else if (!matchA && matchB) {
+    return 1;
+  } else if (matchA && matchB) {
+    let indexA = prioritizedScripts.indexOf(matchA[1]);
+    let indexB = prioritizedScripts.indexOf(matchB[1]);
+    if (indexA === -1) {
+      indexA = Number.MAX_SAFE_INTEGER;
+    }
+    if (indexB === -1) {
+      indexB = Number.MAX_SAFE_INTEGER;
+    }
+    if (indexA === indexB) {
+      return fileA.localeCompare(fileB, 'en-US');
+    }
+    return indexA - indexB;
+  }
+  return fileA.localeCompare(fileB, 'en-US');
+});
+
+let totalTscRuntime = 0;
+let totalJsRunTime = 0;
+
+registerWriteHook((fileName) => {
+  if (
+    toCompileOutputs.includes(basename(fileName)) &&
+    outputDirectories.includes(basename(dirname(fileName)))
+  ) {
+    const t = process.hrtime();
+    const result = spawnSync(process.platform === 'win32' ? 'pnpm.cmd' : 'pnpm', ['build'], {
+      cwd: projectRootDir,
+      stdio: 'inherit',
+      shell: true,
+    });
+    if (result.error) {
+      throw result.error;
+    }
+    const [s, ns] = process.hrtime(t);
+    totalTscRuntime += s + ns / 1e9;
+  }
+});
+
+// Keep track of the runtime of individual scripts to identify performance problems
+const runtime: { [scriptName: string]: number } = {};
+
+for (const tsFile of tsFiles) {
+  const jsFile = path.parse(tsFile).name + '.js';
+  const t = process.hrtime();
+  // Our files are individual scripts, so importing already
+  // has the side effect of running their contents.
+  await import('./' + jsFile);
+  const [s, ns] = process.hrtime(t);
+  runtime[jsFile] = s + ns / 1e9;
+  totalJsRunTime += runtime[jsFile];
+}
+
+//for (const toCopyFile of copyDataToOutput) {
+//  copyFileSync(`./data/${toCopyFile}`, `./output/${toCopyFile}`);
+//}
+
+const runtimes = Object.entries(runtime).sort((a, b) => b[1] - a[1]);
+infoTable(runtimes);
+infoLog(TAG, 'total tsc runtime', totalTscRuntime);
+infoLog(TAG, 'total js runtime', totalJsRunTime);
